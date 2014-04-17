3223d5b96090fd53495cb11e4fdea7653c49f88d
Gabriel <gabi89.m@gmail.com>
2014-04-17 16:30:46 -0300

    Signup widget, ajax preloading, signupt ui service set up

--- public/app/js/main.js
+++ public/app/js/main.js
@@ -15,6 +15,11 @@ app.config(['$interpolateProvider', '$urlRouterProvider', '$stateProvider',
                 templateUrl: '/templates/login.html',
                 controller: 'LoginController'
             })
+            .state('signup', {
+                url: '/signup',
+                templateUrl: '/templates/signup.html',
+                controller: 'SignUpController'
+            })
             .state('wall', {
                 url: '/wall',
                 templateUrl: '/templates/wall.html',
@@ -49,42 +54,83 @@ login.service('SessionService', [function(){
     return { create: create, destroy: destroy, username: username, role: role };
 }]);
 
-login.service('AuthService', ['$q', 'SessionService', function($q, SessionService){
+login.service('AuthService', ['$q', '$timeout', 'SessionService', function($q, $timeout, SessionService){
 
     var login = function(credentials) { // Performs the login of a user
+        if (typeof credentials !== 'object') throw new Error('Credentials argument not valid, needs to be an object');
+        if (!credentials.username || !credentials.password) throw new Error('Credentials argument incomplete, username or password field missing');
         var deferred = $q.defer();
         // Perform server login ...
-        var successLoginResp = {
-                meta: {
-                    success: true,
-                    code: 700,      // 700 Identity authenticated in the server
-                    message: 'Identity successfully authenticated'
+        $timeout(function(){
+            var successLoginResp = {
+                    meta: {
+                        success: true,
+                        code: 700,      // 700 Identity authenticated in the server
+                        message: 'Identity successfully authenticated'
+                    },
+                    data: {
+                        username: credentials.username,
+                        role: 'user'
+                    }
                 },
-                data: {
-                    username: credentials.username,
-                    role: 'user'
-                }
-            },
-            errorLoginResp = {
-                meta: {
-                    success: false,     // A descriptive label for the operation result
-                    code: 600,      // 600: User does not exists
-                    message: 'The user does not exists'     // A message that came from the server
+                errorLoginResp = {
+                    meta: {
+                        success: false,     // A descriptive label for the operation result
+                        code: 600,      // 600: User does not exists
+                        message: 'The user does not exists'     // A message that came from the server
+                    },
+                    data: {
+                        username: credentials.username
+                    }
                 },
-                data: {
-                    username: credentials.username
-                }
-            },
-            loginResp = errorLoginResp; // Select which dummy response Ill use right now
-        if (loginResp.meta.success) {   // Successful logged in user
-            SessionService.create(loginResp.data.username, loginResp.data.role);
-            deferred.resolve({
-                username: SessionService.username,
-                role: SessionService.role
-            });
-        } else {
-            deferred.reject(loginResp.meta);
-        }
+                loginResp = errorLoginResp; // Select which dummy response Ill use right now
+            if (loginResp.meta.success) {   // Successful logged in user
+                SessionService.create(loginResp.data.username, loginResp.data.role);
+                deferred.resolve({
+                    username: SessionService.username,
+                    role: SessionService.role
+                });
+            } else {
+                deferred.reject(loginResp.meta);
+            }
+        }, 1000);
+
+        return deferred.promise;
+    };
+
+    var signUp = function(signUpInfo) {
+        if (typeof signUpInfo !== 'object') throw new Error('The argument for signing up a user is not an object');
+        if (typeof signUpInfo.username !== 'string' || typeof signUpInfo.password !== 'string') { throw new Error('username or password fields are missing from the signup argument ')};
+        var deferred = $q.defer();
+
+        $timeout(function(){
+            var successSignUpResp = {
+                    meta: {
+                        success: true,
+                        code: 710,      // 710 User registered correctly
+                        message: 'The user has been successfully registered'
+                    },
+                    data: {
+                        username: credentials.username
+                    }
+                },
+                errorSignUpResp = {
+                    meta: {
+                        success: false,     // A descriptive label for the operation result
+                        code: 610,      // 600: Username already exists
+                        message: 'The username already exists'     // A message that came from the server
+                    },
+                    data: {
+                        username: credentials.username
+                    }
+                },
+                signUpResp = successSignUpResp; // Select which dummy response Ill use right now
+            if (signUpResp.meta.success) {
+                deferred.resolve();
+            } else {
+                deferred.reject(signUpResp.meta);
+            }
+        }, 1000);
 
         return deferred.promise;
     };
@@ -109,6 +155,7 @@ login.service('AuthService', ['$q', 'SessionService', function($q, SessionServic
 
     return {
         login: login,
+        signUp: signUp,
         credentials: credentials,
         logOut: logOut,
         isAuthorized: isAuthorized,
@@ -135,16 +182,43 @@ login.constant('AUTH_EVENTS', {
 login.controller('LoginController', ['$scope', '$state', 'AuthService', function($scope, $state, AuthService){
     $scope.loginMessages = [];
     $scope.loginStateClass = 'pristine';
+    $scope.loading = false;
     $scope.login = function(credentials) {
+        $scope.loading = true;
         AuthService.login(credentials).then(function(userCredentials){
+            $scope.loading = false;
             if (userCredentials) {
                 $scope.credentials = { username: userCredentials.username, role: userCredentials.role };
                 $state.to('wall');
             }
-        }, function(metaData){
+        }, function(errorInfo){
+            $scope.loading = false;
             $scope.loginStateClass = 'alert alert-danger';
-            $scope.loginMessages = [metaData.message];
-            console.log('Error ', metaData);
+            $scope.loginMessages = [errorInfo.message];
         });
+    }
+}]);
+
+login.controller('SignUpController', ['$scope', '$state', 'AuthService', function($scope, $state, AuthService){
+    $scope.signUpMessages = [];
+    $scope.signUpStateClass = 'pristine';
+    $scope.loading = false;
+    $scope.signUp = function(credentials) {
+        if (credentials.password !== credentials.passwordRep ) {
+            $scope.signUpStateClass = 'alert alert-danger';
+            $scope.signUpMessages = ['Password fields don\'t match'];
+        } else {
+            $scope.loading = true;
+            AuthService.signUp(credentials).then(function(){
+                $scope.loading = false;
+                $scope.signUpStateClass = 'alert alert-success';
+                $scope.signUpMessages = ['Your account has been registered'];
+            }, function(errorInfo){
+                $scope.loading = false;
+                $scope.signUpStateClass = 'alert alert-danger';
+                $scope.signUpMessages = [errorInfo.message];
+            });
+        }
+
     }
 }]);
--- public/test/services/AuthServiceTest.js
+++ public/test/services/AuthServiceTest.js
@@ -1,12 +1,25 @@
 describe('AuthService Test', function() {
     beforeEach(module('m-login'));
+    var authService = null;
 
     it('should contain AuthService', function(){
         inject(function(AuthService){
             expect(AuthService).toBeDefined();
             expect(AuthService).not.toEqual(null);
+            authService = AuthService;
         });
     });
+
+    it('should expose a login deferred method', function(){
+        expect(typeof authService.login).toEqual('function');
+        expect(typeof authService.login({ username: 'user1', password: '123'}).then).toEqual('function');
+    });
+
+    it('should expose a signUp deferred method', function(){
+        expect(typeof authService.signUp).toEqual('function');
+        expect(typeof authService.signUp({ username: 'user1', password: '123', email: 'email@gmail.com'}).then).toEqual('function');
+    });
+
 });
 
 describe('SessionService Test', function(){
